a7067103891276382ecf771aba3ed621d8b5da0a
Arvanhell <arrivald@gmail.com>
2023-07-31 12:15:02 +0100

    sync

--- arraypractice.js
+++ arraypractice.js
@@ -35,13 +35,26 @@
 // Following second
 // Write a program that asks for a time under the form of three information (hours, minutes, seconds). The program calculates and shows the time one second after. Incorrect inputs must be taken into account.
 //   
-let number = 1;
-while (number <= 2) {
-    console.log(number);
-    number++;
-}
+let n = 100
+for (let i = 1; i <= n ; i++ ) {
 
-let num;
-for (number = 1; number<= 6; number++) {
-    console.log(num)
-}
+if  ( i % 3 === 0 && i % 5 === 0) 
+    console.log('FizzBuzz')
+
+else if ( i % 5 === 0 ) 
+    console.log('Buzz')
+
+else if 
+( i % 3 === 0) 
+    console.log('Fizz')
+ 
+  else 
+        console.log(i)
+ }
+
+//  for (let i = 1; i < 101; i++) {
+//     if (i % 15 == 0) console.log("FizzBuzz");
+//     else if (i % 3 == 0) console.log("Fizz");
+//     else if (i % 5 == 0) console.log("Buzz");
+//     else console.log(i);
+// }
